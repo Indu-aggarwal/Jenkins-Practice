6c2809b6420162b715d9c58b84dabcf2bb11100a
Indu <induaggarwa@gmail.com>
2021-06-08 22:42:47 +0100

    Added test3 method in the test java file

--- JenkinsModule/src/test/java/RunnerJenkinsDemoTest.java
+++ JenkinsModule/src/test/java/RunnerJenkinsDemoTest.java
@@ -1,18 +1,21 @@
 import org.junit.jupiter.api.*;
 
-    public class RunnerJenkinsDemo {
+    public class RunnerJenkinsDemoTest {
 
         JenkinsDemo obj = new JenkinsDemo();
 
         @Test
-        public void runTest(){
+        public void test1(){
             obj.test1();
             //System.out.println("Testing my first code");
         }
 
         @Test
-        public void runTest2(){
+        public void test2(){
             System.out.println("Testing my first code");
+        }
+        public void test3(){
+            System.out.println("Testing Jenkins graph");
         }
     }
 
